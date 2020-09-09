d13aeadd723d1433aaba35c0a610638fb0d2200f
mai-gitt <67313239+mai-gitt@users.noreply.github.com>
2020-09-09 14:15:02 +1200

    Add files via upload

---
+++ Model_Training_and_Prediction.py
@@ -0,0 +1,14 @@
+# =======================================
+#  Model Training
+# =======================================
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
+
+adv = pd.read_csv(
+    "https://github.com/cs109/2014_data/blob/master/countries.csv")
+print(adv.head())
+
+# =======================================
